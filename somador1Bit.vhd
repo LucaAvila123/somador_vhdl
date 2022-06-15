23685da26c743f01e1b9a9b9b3c67ff38dab1da3
Luca <91071096+LucaAvila123@users.noreply.github.com>
2022-06-15 18:53:00 -0300

    Update somador1Bit.vhd

--- somador1Bit.vhd
+++ somador1Bit.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity somadorCompleto1Bit is -- esta entidade e sua arquitetura serão usadas como funções
     port(
-        a :          in  std_logic;
+        a :          in  std_logic; 
         b :          in  std_logic;
         cin:         in  std_logic;
         s:           out std_logic;
@@ -16,6 +16,6 @@ architecture arch of somadorCompleto1Bit is
     
 begin
 
-        s <= a xor b xor cin;
+        s <= a xor b xor cin;  
         cout <= (a and b) or (a and cin) or (b and cin);
 end arch;
